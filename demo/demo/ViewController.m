ebc92b753973a70af88595e37ae2374fdee2d461
123 <may_lcc@163.com>
2015-12-01 21:37:07 +0800

    1jk1h111

--- demo/demo/ViewController.m
+++ demo/demo/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSString *str = @"huanghaiayng";
+    NSString *str = @"huang";
     
     // Do any additional setup after loading the view, typically from a nib.
 }
